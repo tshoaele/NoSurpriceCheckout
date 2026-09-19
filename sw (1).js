b9b8481e43746b41f451628bf26416c8fc985724
VujaTsoaela <tshoaele@gmail.com>
2026-09-19 16:33:24 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,32 @@
+var CACHE = "shopping-checklist-v2";
+var FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request).catch(function () { return caches.match("./index.html"); });
+    })
+  );
+});
